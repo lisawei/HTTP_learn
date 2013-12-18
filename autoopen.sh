dc6252b646f7d9cae14eedf98b67379053561156
lisawei <187055815@qq.com>
2013-12-18 20:09:05 +0800

    update

---
+++ autoopen.sh
@@ -0,0 +1,5 @@
+
+open http://zhidao.baidu.com/question/214102034.html?qbl=relate_question_0
+open http://zhidao.baidu.com/question/214102034.html?qbl=relate_question_0
+open http://zhidao.baidu.com/question/214102034.html?qbl=relate_question_0
+open http://zhidao.baidu.com/question/214102034.html?qbl=relate_question_0:
--- http_learn.py
+++ http_learn.py
@@ -1,3 +1,12 @@
-get/post
+math: get, post
 
-Http error code
+Message: request,response.
+
+fomat:
+起始行：要做什么
+首部： name:value
+主体：请求数据 和 返回数据
+
+Http error code：200，304，404，403
+
+https 端口443
